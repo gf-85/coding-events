1c4992099a5c6466f88a4a4c5a90cf41ce634785
Gabi Franz <gabrielafranz85@gmail.com>
2020-05-08 12:25:55 +0200

    Add event property and two binding to title inout field

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h1>Create an Event</h1>
+    <form>
+      <input
+        v-model="event.title"
+        type="text"
+        name="title"
+        placeholder="Add a Title"
+        value="Default title"
+      />
+      <input type="submit" value="Create" />
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      event: {
+        title: ""
+      }
+    };
+  }
+};
 </script>
 
 <style scoped></style>
--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -12,4 +12,47 @@ describe("CreateEvent", () => {
 
     expect(wrapper.get("h1").text()).toBe("Create an Event");
   });
+
+  test("it has an form", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("form")).toBe(true);
+  });
+
+  test("it should contain a title input field", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("input[type='text'][name='title']")).toBe(true);
+  });
+
+  test("it should contain a submit button with the value create", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("input[value='Create'][type='submit']")).toBe(true);
+  });
+
+  test("it should contain an input field for the title with the placeholder 'add a title'", () => {
+    const wrapper = mount(CreateEvent);
+
+    const titleInput = wrapper.get("input[name='title']");
+
+    expect(titleInput.attributes("placeholder")).toBe("Add a Title");
+  });
+
+  test("it should have an event data property", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.vm.event).toEqual({
+      title: ""
+    });
+  });
+
+  test("it should bind the event title to the user input", () => {
+    const wrapper = mount(CreateEvent);
+    const titleInput = wrapper.get("input[name='title']");
+
+    titleInput.setValue("TEST_INPUT");
+
+    expect(wrapper.vm.event.title).toBe("TEST_INPUT");
+  });
 });
