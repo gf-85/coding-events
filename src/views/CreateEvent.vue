bcde10272d163b14b435f08a27989971760f629f
Gabi Franz <gabrielafranz85@gmail.com>
2020-05-09 11:49:41 +0200

    Create POST request on submit

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -25,7 +27,14 @@ export default {
   },
   methods: {
     submit() {
-      console.log(this.event);
+      axios
+        .post("http://localhost:3000/events", this.event)
+        .then(response => {
+          console.log("created new event", response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
 };
